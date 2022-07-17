d35f758463312adf5311637a964fd10a1aa50f87
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2022-07-17 18:47:36 +0200

    Initial commit

---
+++ routes/products.routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+const Product = require('../models/product.model');
+
+router.get('/products', async (req, res) => {
+  try {
+    const result = await Product.find().select('title price');
+    if (!result) res.status(404).json({product: 'Not found'});
+    else res.json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/products/:id', async (req, res) => {
+  try {
+    const result = await Product.findById(req.params.id);
+    if (!result) res.status(404).json({product: 'Not found'});
+    else res.json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
