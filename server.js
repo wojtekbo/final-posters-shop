977ed5780894dacd7792e8bf52b7a878b62fb666
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2022-08-03 02:35:42 +0200

    Fix public folder url

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use('/api', (req, res) => {
 });
 
 /* REACT WEBSITE */
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/client/public')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  res.sendFile(path.join(__dirname, '/client/public/index.html'));
 });
 
 /* MONGOOSE */
