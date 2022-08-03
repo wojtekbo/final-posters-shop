9ca652ccadac4fa2e736d4214c000670f0c1fe75
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2022-08-03 20:23:00 +0300

    Add new data to initialState

--- client/src/redux/initialState.js
+++ client/src/redux/initialState.js
@@ -1,26 +1,45 @@
 const initialState = {
+  selectedProduct: {
+    data: {
+      _id: '62e6cefb2e7b355b27e43192',
+      title: 'Example',
+      description:
+        'Dolor morbi non arcu risus quis varius. Volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris. Sem nulla pharetra diam sit. Facilisis magna etiam tempor orci eu lobortis. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. ',
+      size: [
+        {
+          type: 'Small',
+          dimensions: '25cm x 35cm',
+          price: 15,
+        },
+        {
+          type: 'Medium',
+          dimensions: '50cm x 70cm',
+          price: 20,
+        },
+      ],
+      imgDetails: ['2.png', '3.png', '4.gif'],
+      imgMain: '1.png',
+      author: 'Mariusz Lewczyk',
+      imgMini: '1-mini.jpg',
+      productId: 'psycho_p2mci8',
+    },
+  },
   mainCarouselImgs: {
     data: [
       {
-        _id: {
-          $oid: '62e521b02e7b355b27e4315e',
-        },
+        _id: '62e521b02e7b355b27e4315e',
         img: '3.gif',
         title: 'Psycho',
         productId: 'psycho_p2mci8',
       },
       {
-        _id: {
-          $oid: '62e521e72e7b355b27e4315f',
-        },
+        _id: '62e521e72e7b355b27e4315f',
         img: '4.png',
         title: 'The Shining',
         productId: 'theshining_1a4ms',
       },
       {
-        _id: {
-          $oid: '62e522022e7b355b27e43160',
-        },
+        _id: '62e522022e7b355b27e43160',
         img: '5.png',
         title: 'Get Out',
         productId: 'getout_s4ay7',
@@ -30,9 +49,7 @@ const initialState = {
   products: {
     data: [
       {
-        _id: {
-          $oid: '62d43548226c9447b3ad24b4',
-        },
+        _id: '62d43548226c9447b3ad24b4',
         title: 'Get Out',
         productId: 'getout_s4ay7',
         description:
@@ -55,9 +72,7 @@ const initialState = {
         imgMini: '1-mini.jpg',
       },
       {
-        _id: {
-          $oid: '62e6ce802e7b355b27e43191',
-        },
+        _id: '62e6ce802e7b355b27e43191',
         title: 'The Shining',
         productId: 'theshining_1a4ms',
         description:
@@ -80,9 +95,7 @@ const initialState = {
         imgMini: '1-mini.jpg',
       },
       {
-        _id: {
-          $oid: '62e6cefb2e7b355b27e43192',
-        },
+        _id: '62e6cefb2e7b355b27e43192',
         title: 'Psycho',
         productId: 'psycho_p2mci8',
         description:
